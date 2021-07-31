93d59d9e9a680bb937246af88203c7a9118b7712
John Arundel <john@bitfieldconsulting.com>
2021-07-31 16:34:53 +0100

    Add 3.1, 3.2

---
+++ 3.2/calculator.go
@@ -0,0 +1,15 @@
+// Package calculator provides a library for
+// simple calculations in Go.
+package calculator
+
+// Add takes two numbers and returns the
+// result of adding them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
+
+// Subtract takes two numbers a and b, and
+// returns the result of subtracting b from a.
+func Subtract(a, b float64) float64 {
+	return a - b
+}
