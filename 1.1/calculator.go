0743d2cc9966bef574902905053c557369c2e62d
John Arundel <john@bitfieldconsulting.com>
2021-07-31 16:23:29 +0100

    Add 1.1

---
+++ 1.1/calculator.go
@@ -0,0 +1,9 @@
+// Package calculator provides a library for
+// simple calculations in Go.
+package calculator
+
+// Add takes two numbers and returns the
+// result of adding them together.
+func Add(a, b float64) float64 {
+	return a + b
+}
---
+++ 1.1/calculator_test.go
@@ -0,0 +1,15 @@
+package calculator_test
+
+import (
+	"calculator"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	t.Parallel()
+	var want float64 = 4
+	got := calculator.Add(2, 2)
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
