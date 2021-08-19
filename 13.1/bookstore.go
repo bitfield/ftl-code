ad5966363083b0c3029f23c22e2f4b478a20947f
John Arundel <john@bitfieldconsulting.com>
2021-08-19 14:35:56 +0100

    Add 13.1

---
+++ 13.1/bookstore.go
@@ -0,0 +1,20 @@
+package bookstore
+
+import "errors"
+
+// Book represents information about a book.
+type Book struct {
+	Title  string
+	Author string
+	Copies int
+}
+
+// Buy takes a Book, decrements its number of copies, and returns the modified
+// book.
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("no copies left")
+	    }
+	b.Copies--
+	return b, nil
+}
---
+++ 13.1/bookstore_test.go
@@ -0,0 +1,43 @@
+package bookstore_test
+
+import (
+	"bookstore"
+	"testing"
+)
+
+func TestBook(t *testing.T) {
+	_ = bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondō",
+		Copies: 2,
+	}
+}
+
+func TestBuy(t *testing.T) {
+	input := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondō",
+		Copies: 2,
+	}
+	want := 1
+	result, err := bookstore.Buy(input)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("want %d copies after buying 1 copy from a stock of 2, got %d", want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	input := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondō",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(input)
+	if err == nil {
+		t.Error("want error buying book when zero copies left, but got nil")
+	}
+}
